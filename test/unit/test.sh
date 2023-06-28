4e63ef004e011f9b3838b3160c8ab673530c3a81
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-06-28 22:18:39 +0200

    Introduce serving profile
    The platform provides a "serving" profile to install a serverless
    runtime providing developers with an
    image -> URL experience.
    
    Fixes gh-5
    
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ test/unit/test.sh
@@ -0,0 +1,36 @@
+kapp deploy -a kapp-controller -y \
+  -f https://github.com/carvel-dev/kapp-controller/releases/latest/download/release.yml
+
+kubectl create namespace kadras-packages
+
+kapp deploy -a cert-manager-issuers-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/cert-manager-issuers/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/cert-manager-issuers/releases/latest/download/package.yml
+
+kapp deploy -a cert-manager-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/package-for-cert-manager/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/package-for-cert-manager/releases/latest/download/package.yml
+
+kapp deploy -a contour-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/package-for-contour/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/package-for-contour/releases/latest/download/package.yml
+
+kapp deploy -a knative-serving-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/package-for-knative-serving/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/package-for-knative-serving/releases/latest/download/package.yml
+
+kapp deploy -a metrics-server-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/package-for-metrics-server/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/package-for-metrics-server/releases/latest/download/package.yml
+
+kapp deploy -a secretgen-controller-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/package-for-secretgen-controller/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/package-for-secretgen-controller/releases/latest/download/package.yml
+
+kapp deploy -a workspace-provisioner-package -n kadras-packages -y \
+  -f https://github.com/kadras-io/workspace-provisioner/releases/latest/download/metadata.yml \
+  -f https://github.com/kadras-io/workspace-provisioner/releases/latest/download/package.yml
+
+make prepare
+
+ytt -f ../test/integration/serving/config -f package-resources.yml | kctrl dev -f- --local -y
